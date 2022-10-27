ac611ea4cdba2deffbf22dccb77f4e98167fac3d
Casey Colley <code@caseycolley.com>
2022-10-27 14:17:18 -0700

    copied files for lab 3

---
+++ lab3/lab3.rb
@@ -0,0 +1,29 @@
+#require_relative "element"   # uncomment to load element.rb
+#require_relative "player"    # uncomment to load player.rb 
+#require_relative "history"   # uncomment to load history.rb
+
+######################################### 	
+#     CS 381 - Programming Lab #3		#
+#										#
+#  < your name >					 	#
+#  < your email >	                	#
+#										#
+#########################################
+
+
+
+# your code here
+
+
+
+def game(rounds)
+
+	# your code here
+	
+end
+
+
+# Main Program (should be last)
+n_rounds = 5
+# call to kick off the game
+game(n_rounds)
